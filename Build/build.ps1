86435c1221dd73a5d021ce14fdb4b44ba0c59690
Cameron Taggart <cameron.taggart@gmail.com>
2017-12-29 15:34:12 -0300

    get rid of unnecessary changes

--- Build/build.ps1
+++ Build/build.ps1
@@ -12,8 +12,8 @@
   $msbuildVerbosity = 'minimal'
   $treatWarningsAsErrors = $false
   $workingName = if ($workingName) {$workingName} else {"Working"}
-  # $netCliChannel = "2.0"
-  # $netCliVersion = "2.0.0"
+  $netCliChannel = "2.0"
+  $netCliVersion = "2.0.0"
   $nugetUrl = "http://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 
   $baseDir  = resolve-path ..
@@ -56,6 +56,7 @@ task Clean {
 
   if($env:ci)
   {
+    # for testing CI behavior locally
     Execute-Command -command { git clean -xfd }
   }
   else
@@ -222,7 +223,7 @@ function NetCliTests($build)
   $location = "$workingSourceDir\Newtonsoft.Json.Tests"
   $testDir = if ($build.TestFramework -ne $null) { $build.TestFramework } else { $build.Framework }
 
-  # exec { .\Tools\Dotnet\dotnet-install.ps1 -Channel $netCliChannel -Version $netCliVersion | Out-Default }
+  exec { .\Tools\Dotnet\dotnet-install.ps1 -Channel $netCliChannel -Version $netCliVersion | Out-Default }
 
   try
   {
@@ -235,7 +236,7 @@ function NetCliTests($build)
     Write-Host "Project path: $projectPath"
     Write-Host
 
-    # exec { dotnet test $projectPath -f $testDir -c Release -l trx -r $workingDir --no-restore --no-build | Out-Default }
+    exec { dotnet test $projectPath -f $testDir -c Release -l trx -r $workingDir --no-restore --no-build | Out-Default }
   }
   finally
   {
@@ -254,7 +255,7 @@ function NUnitTests($build)
   try
   {
     Set-Location $testRunDir
-    # exec { & $nunitConsolePath\tools\nunit3-console.exe "$testRunDir\Newtonsoft.Json.Tests.dll" --framework=$framework --result=$workingDir\$testDir.xml --out=$workingDir\$testDir.txt | Out-Default } "Error running $testDir tests"
+    exec { & $nunitConsolePath\tools\nunit3-console.exe "$testRunDir\Newtonsoft.Json.Tests.dll" --framework=$framework --result=$workingDir\$testDir.xml --out=$workingDir\$testDir.txt | Out-Default } "Error running $testDir tests"
   }
   finally
   {
